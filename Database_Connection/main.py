a40a7a5b1231efc38fb7bc2837e8897bdefa0347
originalop <rswami5579@gmail.com>
2025-09-04 00:52:45 +0530

    Styled and fixed typo

--- Database_Connection/main.py
+++ Database_Connection/main.py
@@ -4,14 +4,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Insert data function
+def insert_data(supabase_obj, table_name, sr_num, movie_name, watched_date):
+    response = (
+    supabase_obj.table(f"{table_name}")
+    .insert({"sr": f"{sr_num}", "movie_name": f"{movie_name}", "watched_date":f"{watched_date}"})
+    .execute()
+)
 
+# Delete data
+def delete_date():
+    response = (
+    supabase.table("countries")
+    .delete()
+    .eq("id", 1)
+    .execute()
+)
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 name: str = os.environ.get("name")
 supabase: Client = create_client(url, key)
 
-response = (
-    supabase.table("Movies")
-    .insert({"sr": 1, "movie_name": "Mission impossible: Final reckoning", "watched_date":"2025-09-01"})
-    .execute()
-)
+for i in range(4,1001):
+    insert_data(supabase, "Movies", i, "Spider-man", "2025-09-03")
