7f228ef7e8a66e6b1f29036a9b480b3ba72f5d09
originalop <rswami5579@gmail.com>
2025-09-06 11:24:05 +0530

    Added security level

--- Database_Connection/main.py
+++ Database_Connection/main.py
@@ -13,18 +13,25 @@ def insert_data(supabase_obj, table_name, sr_num, movie_name, watched_date):
     .execute()
 )
 
-# Delete data
-def delete_date():
+# Delete data. Takes 3 parameters. `Table name` and `column name` and `column id`
+def delete_date(table_name, column_name, column_id):
     response = (
-    supabase.table("countries")
+    supabase.table(table_name)
     .delete()
-    .eq("id", 1)
+    .eq(column_name, column_id)
     .execute()
 )
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 name: str = os.environ.get("name")
 supabase: Client = create_client(url, key)
+TABLE_NAME="Movies"
+TABLE_ATTRIBUTES=["sr", "movie_name", "watched_date"]
 
-for i in range(4,1001):
-    insert_data(supabase, "Movies", i, "Spider-man", "2025-09-03")
+# Insert data function call
+# for i in range(4,1001):
+    # insert_data(supabase, "Movies", i, "Spider-man", "2025-09-03")
+
+# Delete data functino call
+# for i in range(1,1001):
+#     delete_date(TABLE_NAME, TABLE_ATTRIBUTES[0], i)
