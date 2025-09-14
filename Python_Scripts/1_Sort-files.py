2cba47a9fe54f6c6a0b8e01c080223fb61538565
pagebase <rswami5579@gmail.com>
2025-09-14 21:45:12 +0530

    Added try and except block properly

--- Python_Scripts/1_Sort-files.py
+++ Python_Scripts/1_Sort-files.py
@@ -1,9 +1,10 @@
 import os
 import shutil
 
-try:
-    # This function handle duplicate files
-    def handle_duplicate_file(dst, directory_name, file_name):
+
+# This function handle duplicate files
+def handle_duplicate_file(dst, directory_name, file_name):
+    try:
         name, ext = os.path.splitext(file_name)
         counter = 1
         new_file_name = f"{name}_{counter}{ext}"
@@ -15,15 +16,20 @@ try:
             new_file_path = os.path.join(dst, directory_name, new_file_name)
 
         return new_file_path
+    except Exception as e:
+        print(e)
 
-
-    # Create directories function
-    def create_directory(directory_names, dst):
+# Create directories function
+def create_directory(directory_names, dst):
+    try:
         for i in directory_names:
             path = os.path.join(dst,i) # If source path is `C:\Sorted_Data` then `path` variable contains `C:\Sorted_Data\Photos`, `C:\Sorted_Data\Videos` etc
             os.makedirs(path, exist_ok=True) # 2nd parameter would not raise FileExistEror. If folder already exist then it will not create it and won't raise any error.
+    except Exception as e:
+        print(e)
 
-    def move_files(src, dst, file_ext_list, directory_name):
+def move_files(src, dst, file_ext_list, directory_name):
+    try:
         for root, folders, files in os.walk(src):
             for file in files:
                 if file.lower().endswith(tuple(file_ext_list)): # `endswith()` method takes `str` or `tuple of str` as argument.
@@ -34,15 +40,18 @@ try:
                         shutil.move(full_src_path, duplicate_full_path)
                     else:
                         shutil.move(full_src_path, full_dst_path)
+    except Exception as e:
+        print(e)
 
+try:
     src=input("Enter source path: ").strip() # `strip()` method remove white spaces from left, right sides.
     user_choice=input("Source and destination path is same?(yes | no): ").strip().lower()
     if user_choice=="Yes".lower():
         dst=src
     else:
-        dst=input("Enter destination path: ").strip()
+        dst=input("Enter destination path: ").strip() # `strip()` method remove white spaces from left side and right side.
     folder_names = ["Photos", "Videos", "Audio", "Documents", "Softwares", "Archives"]
-    img_ext_list = [".png", ".jpeg", ".jpg", ".webp"]
+    img_ext_list = [".png", ".jpeg", ".jpg", ".webp", ".gif"]
     vid_ext_list = [".mp4", ".mkv", ".mov", ".wmv"]
     aud_ext_list = [".mp3", ".wav", ".aac"]
     doc_ext_list = [".pdf", ".csv", ".docx", ".pptx", ".txt", ".md", ".torrent"]
