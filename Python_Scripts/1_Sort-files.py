33863ef7db787a9de308bc8572105ae286a832da
Mr. Cat <163742860+pagebase@users.noreply.github.com>
2025-09-20 19:14:26 +0530

    Added excel extension.

--- Python_Scripts/1_Sort-files.py
+++ Python_Scripts/1_Sort-files.py
@@ -54,7 +54,7 @@ try:
     img_ext_list = [".png", ".jpeg", ".jpg", ".webp", ".gif"]
     vid_ext_list = [".mp4", ".mkv", ".mov", ".wmv"]
     aud_ext_list = [".mp3", ".wav", ".aac"]
-    doc_ext_list = [".pdf", ".csv", ".docx", ".pptx", ".txt", ".md", ".torrent"]
+    doc_ext_list = [".pdf", ".csv", ".docx", ".pptx", ".txt", ".md", ".torrent", ".xlsx"]
     soft_ext_list=[".exe"]
     arc_ext_list=[".zip"]
     counter=1
