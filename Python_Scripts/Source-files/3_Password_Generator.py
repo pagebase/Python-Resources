2fd2aec358f47ed7057eddf9c78826e373e748b0
Mr. Cat <163742860+pagebase@users.noreply.github.com>
2025-12-01 06:05:45 +0530

    Refactor password generation and clipboard functionality
    Fixed password string construction and clipboard copying.

--- Python_Scripts/Source-files/3_Password_Generator.py
+++ Python_Scripts/Source-files/3_Password_Generator.py
@@ -6,6 +6,7 @@ lower_letters=[chr(letter) for letter in range(97, 123)]
 numbers=[chr(num) for num in range(48, 58)]
 symbols=[chr(sym) for sym in range(33, 48)]
 password_list=[]
+password_string=""
 
 # Ask user how many upper letters he/she wants
 upper_letter_choice=int(input("How many upper case do you want?: ").strip())
@@ -27,12 +28,15 @@ for i in range(1,numbers_choice+1):
 
 # Add symbols
 for i in range(1,symbol_choice+1):
-    password_list+=symbols[random.randint(0,len(symbols)-1)]
+    password_list.append(symbols[random.randint(0,len(symbols)-1)])
 
 # shuffle element from list
 random.shuffle(password_list)
 
+# Move list element into string
+for char in password_list:
+    password_string+=char
+
 # Copy password_list into clipboard
-random.shuffle(password_list)
-pyperclip.copy(password_list)
+pyperclip.copy(password_string)
 print("Successfully copid password_list into your clipboard")
